1811049e5f32fb0479e50cfff654846d09b72070
Dom Eccleston <domeccleston@gmail.com>
2020-07-14 22:31:35 +0100

    start on design patterns

---
+++ maps/src/index.ts
@@ -0,0 +1,11 @@
+import { User } from './User';
+import { Company } from './Company';
+import {} from '@types/googlemaps';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
+
+google
